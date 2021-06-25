55573caf22e4f4ebdb709b411d2fa6cb52b610c7
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-25 16:41:09 -0500

    Update themes.js

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,13 +1,11 @@
 export const lightTheme = {
-    body: "#E2E2E2",
+    main: "#E2E2E2",
     text: "#363537",
-    toggleBorder: "#FFF",
-    gradient: "linear-gradient(#39598A, #79D7ED)",
 };
 
 export const darkTheme = {
-    body: "#363537",
-    text: "#FAFAFA",
-    toggleBorder: "#6B8096",
-    gradient: "linear-gradient(#091236, #1E215D)",
+    main: "#222831",
+    text: "#EEEEEE",
+    accent1: "#393E46",
+    accent2: "#00ADB5",
 };
