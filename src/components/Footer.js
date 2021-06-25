54c410ff4da807bbcdc257c185d386828dda15a7
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-25 16:41:18 -0500

    test styling on header/footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+    width: 100%;
+    margin: 0 auto;
+    background-color: ${(props) => props.theme.main};
+    border-top: 10px solid ${(props) => props.theme.accent1};
+    border-bottom: 1px solid ${(props) => props.theme.main};
+`;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: ${(props) => props.theme.text};
+`;
 
 const Footer = () => {
     return (
         <>
-            <footer>FOOTER</footer>
+            <StyledFooter>
+                <Title>Footer Text Goes Here</Title>
+            </StyledFooter>
         </>
     );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    width: 100%;
+    margin: 0 auto;
+    background-color: ${(props) => props.theme.main};
+    border-top: 1px solid ${(props) => props.theme.main};
+    border-bottom: 10px solid ${(props) => props.theme.accent1};
+`;
+
+const Title = styled.h1`
+    font-size: 3em;
+    text-align: center;
+    color: ${(props) => props.theme.text};
+`;
 
 const Header = () => {
     return (
         <>
-            <header>HEADER</header>
+            <StyledHeader>
+                <Title>Header Text Goes Here</Title>
+            </StyledHeader>
         </>
     );
 };
