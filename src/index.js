f426fd1c6c1cbf47c8b4b1586abe5ecd3343d97b
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-25 15:43:44 -0500

    index.js: update index.css import and import ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "normalize.css";
-import "./index.css";
+import "./styles/index.css";
+import ThemeProvider from "styled-components";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
