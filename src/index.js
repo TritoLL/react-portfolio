9a7fc681402db396124d8f663291d03b9b44c626
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-25 16:07:37 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "normalize.css";
 import "./styles/index.css";
-import ThemeProvider from "styled-components";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
