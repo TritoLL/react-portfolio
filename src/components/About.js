235a2d161013845596ee987be3352fec25a2dfcc
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-25 15:19:24 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const About = () => {
-    return <div>About Page</div>;
+    return (
+        <>
+            <div>test</div>
+        </>
+    );
 };
 
 export default About;
