937ed41c47d150d7ecc7663ffff7c9732e6d58d3
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-25 14:54:01 -0500

    format with Prettier

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
-import './App.css';
-import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
-import Home from "./components/Home"
-import PageNotFound from "./components/PageNotFound"
+import "./App.css";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from "react-router-dom";
+import Home from "./components/Home";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
-  return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/404" component={PageNotFound}/>
-          <Redirect to="/404" />
-        </Switch>
-      </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/404" component={PageNotFound} />
+                <Redirect to="/404" />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { About, Education, Projects } from "./";
 
 const Home = () => {
-    return(
-    <div>Hello World!</div>
+    return (
+        <>
+            <div> Hello World! </div> <About />
+            <Education />
+            <Projects />
+        </>
     );
-}
+};
 
 export default Home;
