ea080e02bbc019c8e7d816040ecf891e86229e41
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-25 14:44:38 -0500

    Add Router and Switch to redirect to 404 page if needed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import Home from "./components/Home"
+import PageNotFound from "./components/PageNotFound"
 
 function App() {
   return (
-    <p>Hello World!</p>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/404" component={PageNotFound}/>
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
   );
 }
 
