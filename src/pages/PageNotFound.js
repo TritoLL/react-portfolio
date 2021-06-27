48f39f7982637309dae9ad2cb31f08f70eec7618
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-27 16:48:53 -0500

    better 404 page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Oops = styled.h1`
+    font-size: 10em;
+`;
+
+const OopsParagraph = styled.p`
+    padding: 10px;
+    font-size: 5em;
+    text-align: center;
+`;
+
+const CenteredDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+`;
 
 const PageNotFound = () => {
-    return(
-    <div>404 Page Not Found</div>
+    return (
+        <CenteredDiv>
+            <div>
+                <Oops>Oops!</Oops>
+                <OopsParagraph>
+                    There's nothing here.<br></br>
+                    <Link to="/">Go home?</Link>
+                </OopsParagraph>
+            </div>
+        </CenteredDiv>
     );
-}
+};
 
 export default PageNotFound;
