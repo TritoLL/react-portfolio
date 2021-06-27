9e0f4a8991515b9b7b5bc91dc2c330f568ff8105
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-27 17:09:26 -0500

    separate icon links into their own component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import IconLinks from "./IconLinks";
 
 const Footer = () => {
     return (
         <>
             <footer>
-                <a href="https://www.linkedin.com/in/afriesen95/">
-                    <FaLinkedin size={50} />
-                </a>
-
-                <a href="https://github.com/tritoLL">
-                    <FaGithub size={50} />
-                </a>
+                <IconLinks />
             </footer>
         </>
     );
---
+++ src/components/IconLinks.js
@@ -0,0 +1,15 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+const IconLinks = () => (
+    <>
+        <a href="https://www.linkedin.com/in/afriesen95/">
+            <FaLinkedin size={50} />
+        </a>
+
+        <a href="https://github.com/tritoLL">
+            <FaGithub size={50} />
+        </a>
+    </>
+);
+
+export default IconLinks;
