fa232216f99c6b5927dc08324762e0c865a54883
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-28 15:11:48 -0500

    fix horizontal scrolling from bad header/footer padding

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -15,7 +15,7 @@ export const GlobalStyle = createGlobalStyle`
 
     header, footer {
         background-color: ${(props) => props.theme.main};
-        padding: 10px;
+        padding: 10px 0px;
         width: 100%;
         margin: 0 auto;
     }
