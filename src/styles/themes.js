840fa84f65ce4891afaac794ed93ee355472698d
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-28 15:20:39 -0500

    change theme colors

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,13 +1,13 @@
 export const lightTheme = {
     main: "#7868E6",
-    body: "#E4FBFF",
+    body: "#393556",
     border: "#B8B5FF",
-    text: "#EDEEF7",
+    text: "#EEEEEE",
 };
 
 export const darkTheme = {
     main: "#222831",
     body: "#393E46",
-    border: "#00ADB5",
-    text: "#EEEEEE",
+    border: "#00ADC2",
+    text: "#D5D5D5",
 };
