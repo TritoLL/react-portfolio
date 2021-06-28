75d4a164126a14db42a1a28b17aa5b49f1275cbf
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-28 16:58:16 -0500

    placeholder content for AboutThis

--- src/components/AboutThis.js
+++ src/components/AboutThis.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import { AnchorTitle } from "../styles/CustomStyles";
 
 const AboutThis = () => {
-    return <section>About This Portfolio</section>;
+    return (
+        <section>
+            <h1>
+                <AnchorTitle href="#portfolio">About This Website</AnchorTitle>
+            </h1>
+            <p>Bla</p>
+            <p>Bla</p>
+            <p>Bla</p>
+            <p>Bla</p>
+            <p>Bla</p>
+        </section>
+    );
 };
 
 export default AboutThis;
