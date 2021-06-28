8a96db1da7bf0f8eb70d9f802363bf8ca257b563
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-28 15:30:54 -0500

    add content and styling to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,58 @@
 import React from "react";
+import styled from "styled-components";
+
+const bigText = "5";
+const smallText = "2";
+
+const CodeParagraph = styled.p`
+    font-size: ${(props) => props.fontSize * 1.5}vw;
+    position: relative;
+    width: 80%;
+    left: ${(props) => props.left - 15}%;
+    :nth-child(even) {
+        color: ${(props) => props.theme.border};
+    }
+
+    @media (min-width: 768px) {
+        width: ${(props) => props.width}%;
+        left: ${(props) => props.left}%;
+        font-size: ${(props) => props.fontSize}vw;
+    }
+`;
+
+const Key = styled.span`
+    color: ${(props) => props.theme.border};
+    font-size: ${smallText * 1.25}vw;
+
+    @media (min-width: 768px) {
+        font-size: ${smallText * 0.75}vw;
+    }
+`;
 
 const About = () => {
     return (
         <>
-            <div>About</div>
+            <CodeParagraph fontSize={bigText} left="25" width="55">
+                const <b>aboutMe</b> = &#123;
+            </CodeParagraph>
+            <CodeParagraph fontSize={smallText} left="30" width="50">
+                <Key>myName:</Key> 'Alex',
+                <br />
+                <Key>myAge:</Key> 'xx years, xx months, xx days',
+                <br />
+                <Key>myLocation:</Key> 'Manitoba, Canada',
+                <br />
+                <Key>myInterests:</Key> ['Game Development', 'Software
+                Development', 'Web Development', 'Video Games', 'Cute Dogs'],
+                <br />
+                <Key>aComment:</Key> 'Welcome to my portfolio! Here you'll find
+                information about me, this website, my personal projects, and my
+                education.',
+                <br />
+            </CodeParagraph>
+            <CodeParagraph fontSize={bigText} left="25" width="10">
+                &#125;
+            </CodeParagraph>
         </>
     );
 };
