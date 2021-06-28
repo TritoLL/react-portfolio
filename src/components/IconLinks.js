befc949e61762a0e914c39f251bad8315c0cb478
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-28 14:26:13 -0500

    change icon size to a variable

--- src/components/IconLinks.js
+++ src/components/IconLinks.js
@@ -1,18 +1,20 @@
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import styled from "styled-components";
 
+const iconSize = 50;
+
 const PaddedLink = styled.a`
     padding: 0px 5px;
 `;
 
 const IconLinks = () => (
     <div className="icon-links">
-        <PaddedLink href="https://www.linkedin.com/in/afriesen95/">
-            <FaLinkedin size={50} />
+        <PaddedLink href="https://github.com/TritoLL">
+            <FaGithub size={iconSize} />
         </PaddedLink>
 
-        <PaddedLink href="https://github.com/TritoLL">
-            <FaGithub size={50} />
+        <PaddedLink href="https://www.linkedin.com/in/afriesen95/">
+            <FaLinkedin size={iconSize} />
         </PaddedLink>
     </div>
 );
