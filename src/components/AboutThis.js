889972d1c5a3759d8a5f8589c4ca1a91c1f82749
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-06-30 18:50:48 -0500

    add AboutThis content

--- src/components/AboutThis.js
+++ src/components/AboutThis.js
@@ -1,18 +1,48 @@
 import React from "react";
 import styled from "styled-components";
-import { AnchorTitle } from "../styles/CustomStyles";
+import {
+    AnchorTitle,
+    CenteredDiv,
+    CustomParagraph,
+} from "../styles/CustomStyles";
+import { FaReact, FaNpm } from "react-icons/fa";
 
-const AboutThis = () => {
+const IconBox = styled.div`
+    display: flex;
+    padding: 20px 0px;
+    width: 100%;
+    justify-content: center;
+
+    * {
+        padding: 0px 15px;
+    }
+`;
+
+const AboutThis = ({ iconSize = 100 }) => {
     return (
         <section>
             <h1>
                 <AnchorTitle href="#portfolio">About This Website</AnchorTitle>
             </h1>
-            <p>Bla</p>
-            <p>Bla</p>
-            <p>Bla</p>
-            <p>Bla</p>
-            <p>Bla</p>
+            <CenteredDiv>
+                <CustomParagraph size="small">
+                    This portfolio was created during the break between my
+                    second and third terms of study at Red River College. It is
+                    a portfolio piece in itself, as it is my first real website
+                    built with React.
+                </CustomParagraph>
+                <CustomParagraph
+                    size="small"
+                    style={{ fontWeight: "bold", textAlign: "center" }}
+                >
+                    Technologies used:
+                </CustomParagraph>
+            </CenteredDiv>
+
+            <IconBox>
+                <FaReact size={iconSize} />
+                <FaNpm size={iconSize} />
+            </IconBox>
         </section>
     );
 };
