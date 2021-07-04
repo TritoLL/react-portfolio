c8ece74792d27c919fbfa1e1927270716bafbeb2
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-07-04 16:27:02 -0500

    move education info to JSON format

---
+++ src/data/EducationEntries.js
@@ -0,0 +1,31 @@
+export const educationEntries = [
+    {
+        institutionName: "Red River College",
+        courseName: "Business Information Technology",
+        startDate: "Sep 2020",
+        endDate: "Present",
+        bulletPoints: [
+            "Learned object oriented programming concepts such as encapsulation, inheritance, and polymorphism.",
+            "Applied more advanced concepts such as data binding, collections, and generics.",
+            "Performed unit testing in MSTest framework.",
+            "Built a CRUD application using Windows Forms.",
+            "Acquired SQL knowledge including CRUD/ERD basics, table normalization, functions and aggregates.",
+            "Used various languages such as C#, JavaScript, and SQL.",
+            "Built and troubleshot (yes, that's really the past tense of troubleshoot) computer networks in Cisco Packet Tracer.",
+            "Created analysis and design models of various systems using Astah software.",
+            "Designed, implemented, and deployed a website built from scratch.",
+        ],
+    },
+    {
+        institutionName: "Red River College",
+        courseName: "Introduction to Business Information Technology (Honours)",
+        startDate: "Jan 2020",
+        endDate: "May 2020",
+        bulletPoints: [
+            "Acquired proficiency in Microsoft Excel and Microsoft Word.",
+            "Learned fundamental programming concepts such as boolean expressions, conditional statements, and looping.",
+            "Learned number systems such as binary, octal and hexadecimal.",
+            "Honed soft skills via presentations and group work.",
+        ],
+    },
+];
