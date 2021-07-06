243d37e7240e6560feddc4b36054473f0c642a01
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-07-06 18:38:28 -0500

    add 5 placeholder slides

---
+++ src/data/ProjectsData.js
@@ -0,0 +1,32 @@
+export const projectsData = [
+    {
+        title: "Slide 1",
+        description: "Description 1",
+        img: "../images/testimage.png",
+        alt: "test",
+    },
+    {
+        title: "Slide 2",
+        description: "Description 2",
+        img: "../images/testimage.png",
+        alt: "test",
+    },
+    {
+        title: "Slide 3",
+        description: "Description 3",
+        img: "../images/testimage.png",
+        alt: "test",
+    },
+    {
+        title: "Slide 4",
+        description: "Description 4",
+        img: "../images/testimage.png",
+        alt: "test",
+    },
+    {
+        title: "Slide 5",
+        description: "Description 5",
+        img: "../images/testimage.png",
+        alt: "test",
+    },
+];
