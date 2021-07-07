7310304ab203c255f78ae54cb5b103dec286e659
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-07-06 19:55:47 -0500

    add link key to ProjectsData

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -3,6 +3,7 @@ export const projectsData = [
         title: "Tap Those Targets",
         description:
             "Tap Those Targets is a small video game created in Unity with C#. I developed it over the course of three weeks during a break between my first and second terms of Business Information Technology. Tap Those Targets is published on Steam and Google Play.",
+        link: "https://store.steampowered.com/app/1514200/Tap_Those_Targets/",
         img: "../images/tapthosetargets.png",
         alt: "Tap Those Targets",
     },
@@ -10,6 +11,7 @@ export const projectsData = [
         title: "DehydratedBeanSand.com",
         description:
             "When tasked with creating a website from scratch as my final project of Web Development 1, I let my imagination wander to thoughts of dehydrated beans. This site was made entirely with vanilla HTML/CSS/JS. The domain no longer goes anywhere, but the website lives on in my heart.",
+        link: "https://www.DehydratedBeanSand.com",
         img: "../images/dehydratedbeansand.png",
         alt: "DehydratedBeanSand.com",
     },
