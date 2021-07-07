aa90703ef17d78d2094a5e3132ca0ec13b7a99b4
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-07-07 17:16:33 -0500

    add extremely important cute dog pictures to 404 page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+const CuteDogImage = styled.img`
+    position: fixed;
+    display: inline;
+    z-index: -1;
+    border-radius: 50%;
+    top: -100%;
+    width: 10%;
+`;
 
 const Oops = styled.h1`
     font-size: 10em;
     color: ${(props) => props.theme.border};
+    text-shadow: 6px 6px 5px black;
 `;
 
 const OopsParagraph = styled.p`
     padding: 10px;
     font-size: 5em;
     text-align: center;
+    text-shadow: 6px 6px 5px black;
 `;
 
 const PageCenteredDiv = styled.div`
@@ -22,17 +33,37 @@ const PageCenteredDiv = styled.div`
     height: 100%;
 `;
 
+const moveDogImages = () => {
+    const dogImages = document.getElementsByTagName("img");
+
+    for (let i = 0; i < dogImages.length; i++) {
+        dogImages[i].style.top = `${Math.floor(Math.random() * 100)}%`;
+        dogImages[i].style.left = `${Math.floor(Math.random() * 100)}%`;
+        dogImages[i].style.transform = `rotate(${Math.floor(
+            Math.random() * 360
+        )}deg)`;
+    }
+};
+
 const PageNotFound = () => {
+    useEffect(() => {
+        setInterval(moveDogImages, 175);
+    }, []);
+
     return (
-        <PageCenteredDiv>
-            <div>
-                <Oops>Oops!</Oops>
-                <OopsParagraph>
-                    There's nothing here.<br></br>
-                    <Link to="/">Go home?</Link>
-                </OopsParagraph>
-            </div>
-        </PageCenteredDiv>
+        <>
+            <CuteDogImage src="../images/belle.jpg" alt="Belle The Dog" />
+            <CuteDogImage src="../images/mindy.jpg" alt="Mindy The Dog" />
+            <PageCenteredDiv>
+                <div>
+                    <Oops>Oops!</Oops>
+                    <OopsParagraph>
+                        There's nothing here.<br></br>
+                        <Link to="/">Go home?</Link>
+                    </OopsParagraph>
+                </div>
+            </PageCenteredDiv>
+        </>
     );
 };
 
