fc0f00ed50e9cc1c1b43f11641b0ecb4e2ba8434
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-08-03 15:51:46 -0500

    remove footer from TicTacDogPage

--- src/pages/TicTacDogPage.js
+++ src/pages/TicTacDogPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ScrollToTop from "../components/ScrollToTop";
 import TicTacDog from "../components/TicTacDog";
@@ -9,7 +8,6 @@ const TicTacDogPage = ({ theme, toggleTheme }) => {
         <>
             <Header theme={theme} toggleTheme={toggleTheme} />
             <TicTacDog />
-            <Footer />
             <ScrollToTop />
         </>
     );
