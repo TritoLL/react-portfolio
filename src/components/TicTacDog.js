88bd302ba7c957902813f7d3f8a2fce7f8aef41b
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-08-03 15:04:11 -0500

    add TicTacDog component
    this will be the actual game

---
+++ src/components/TicTacDog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TicTacDog = () => {
+    return (
+        <>
+            <p>Tic Tac Dog goes here...</p>
+        </>
+    );
+};
+
+export default TicTacDog;
--- src/pages/TicTacDogPage.js
+++ src/pages/TicTacDogPage.js
@@ -2,12 +2,13 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ScrollToTop from "../components/ScrollToTop";
+import TicTacDog from "../components/TicTacDog";
 
 const TicTacDogPage = ({ theme, toggleTheme }) => {
     return (
         <>
             <Header theme={theme} toggleTheme={toggleTheme} />
-            tic tac dog goes here...
+            <TicTacDog />
             <Footer />
             <ScrollToTop />
         </>
