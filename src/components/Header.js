543bcc61c0f5a8efabef7e6e4e84e794b5f6a0b6
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-08-03 14:56:49 -0500

    clicking my name in header now leads to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import IconLinks from "./IconLinks";
 import ThemeToggler from "./ThemeToggler";
 import FlavorTextRotator from "./FlavorTextRotator";
-import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { HeaderFooter, MobileIconsContainer } from "../styles/CustomStyles";
 
 const CustomHeader = styled(HeaderFooter)`
     border-bottom: 7px solid ${(props) => props.theme.border};
+
+    a {
+        text-decoration: none;
+    }
 `;
 
 const Header = ({ theme, toggleTheme }) => {
     return (
         <CustomHeader>
-            <h1>Alex Friesen</h1>
+            <h1>
+                <Link to="/">Alex Friesen</Link>
+            </h1>
+
             <h2>
                 <FlavorTextRotator />
             </h2>
