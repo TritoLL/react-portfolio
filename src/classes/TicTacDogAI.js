1a2ef60f2f7ff12925b28bf608ca294479929c60
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-08-05 17:41:20 -0500

    TicTacDog: AI can determine an array of true/false/null based on game square states

--- src/classes/TicTacDogAI.js
+++ src/classes/TicTacDogAI.js
@@ -4,6 +4,18 @@ const moveDelay = 1000;
 class TicTacDogAI {
     static makeMove = async (images) => {
         await new Promise((r) => setTimeout(r, moveDelay));
+        return this.#determineMove(images);
+    };
+
+    static #determineMove = (images) => {
+        const validImages = [...images].map((img) =>
+            img.getAttribute("src").includes("belle")
+                ? false
+                : img.getAttribute("src").includes("mindy")
+                ? true
+                : null
+        );
+        console.log(validImages);
         return images[0];
     };
 }
