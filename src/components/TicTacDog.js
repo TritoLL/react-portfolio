6fea3da9cf9486fe4fdc3a64898fee1ae53da0c0
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-08-06 16:36:22 -0500

    TicTacDog: export the winning combinations array

--- src/components/TicTacDog.js
+++ src/components/TicTacDog.js
@@ -2,6 +2,20 @@ import React, { useContext, useState } from "react";
 import styled, { ThemeContext } from "styled-components";
 import TicTacDogAI from "../classes/TicTacDogAI";
 
+//[0][1][2]
+//[3][4][5]
+//[6][7][8]
+export const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 const debugging = false;
 const gapSize = 20;
 const tileBorderSize = 5;
@@ -142,20 +156,6 @@ const clearAllImages = () => {
 const findNameOfWinner = () => {
     // find the "src" attribute of all images
     const imagePaths = allGameSquareImagePaths();
-
-    //[0][1][2]
-    //[3][4][5]
-    //[6][7][8]
-    const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
 
     for (let i = 0; i < winningCombinations.length; i++) {
         // if any of the squares we are checking are empty, check the next combination immediately
