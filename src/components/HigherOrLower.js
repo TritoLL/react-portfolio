fea7323d67ff13e983bb2c7fdcb743dd9f9ba19e
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-08-26 18:27:43 -0500

    HigherOrLower: more styled components

--- src/components/HigherOrLower.js
+++ src/components/HigherOrLower.js
@@ -16,11 +16,73 @@ const ErrorHeading = styled.h1`
 
 const CardImage = styled.img`
     border: 5px solid ${(props) => props.theme.border};
-    border-radius: 7%;
+    border-radius: 5%;
     padding: 2px;
-    margin: 5px;
-    height: 314px;
-    width: 226px;
+    margin: 0px 25px;
+`;
+
+const GameContainer = styled.div`
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+        margin-top: 25px;
+    }
+`;
+
+const GameControls = styled.div`
+    display: flex;
+    order: 3;
+
+    @media (min-width: 768px) {
+        order: 2;
+        flex-direction: column;
+    }
+`;
+
+const GameButton = styled.button`
+    box-sizing: border-box;
+    padding: 5px;
+    margin: 10px;
+    border-radius: 5%;
+    font-size: 2em;
+    background-color: ${(props) => props.theme.main};
+    color: ${(props) => props.theme.text};
+    border: 2px solid ${(props) => props.theme.border};
+
+    :hover,
+    :focus {
+        background-color: ${(props) => props.theme.body};
+    }
+
+    :active {
+        background-color: ${(props) => props.theme.main};
+    }
+
+    @media (min-width: 768px) {
+        margin: 10px 0;
+    }
+`;
+
+const FirstCardImage = styled(CardImage)`
+    order: 1;
+
+    @media (min-width: 768px) {
+        order: 1;
+    }
+`;
+
+const SecondCardImage = styled(CardImage)`
+    order: 2;
+
+    @media (min-width: 768px) {
+        order: 3;
+    }
 `;
 
 const HigherOrLower = () => {
@@ -113,25 +175,20 @@ const HigherOrLower = () => {
                 Guess if the next card will be higher or lower than the current
                 card.
             </GameInstructions>
-            <CardImage
-                src={firstCard.image}
-                alt={`${firstCard.value} ${firstCard.suit}`}
-            />
-            <CardImage
-                src={secondCard.image}
-                alt={`${secondCard.value} ${secondCard.suit}`}
-            />
-            <button
-                onClick={async () => {
-                    let firstCard = await nextCard();
-                    let secondCard = await nextCard();
-
-                    setFirstCard(firstCard);
-                    setSecondCard(secondCard);
-                }}
-            >
-                Draw a Card
-            </button>
+            <GameContainer>
+                <FirstCardImage
+                    src={firstCard.image}
+                    alt={`${firstCard.value} ${firstCard.suit}`}
+                />
+                <GameControls>
+                    <GameButton>Higher</GameButton>
+                    <GameButton>Lower</GameButton>
+                </GameControls>
+                <SecondCardImage
+                    src={secondCard.image}
+                    alt={`${secondCard.value} ${secondCard.suit}`}
+                />
+            </GameContainer>
         </>
     );
 };
