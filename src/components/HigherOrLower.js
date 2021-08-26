97f47e08f7abba6d61361f9ce5e2e2a58d882151
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-08-26 15:31:19 -0500

    HigherOrLower: fetch new deck and display error message if needed

--- src/components/HigherOrLower.js
+++ src/components/HigherOrLower.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 const HigherOrLower = () => {
-    return "Hello World!";
+    const [deckID, setDeckID] = useState("");
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        try {
+            fetch(
+                "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+            )
+                .then((response) => response.json())
+                .then((data) => setDeckID(data["deck_id"]));
+        } catch (error) {
+            setError(
+                "Something went wrong when fetching a new deck.\nPlease try refreshing the page."
+            );
+        }
+    }, []);
+
+    let errorMessage;
+    if (error !== "") errorMessage = `<h1 style="color:red;">${error}</h1>`;
+
+    return (
+        <>
+            {errorMessage}
+            <h1>{deckID}</h1>
+        </>
+    );
 };
 
 export default HigherOrLower;
